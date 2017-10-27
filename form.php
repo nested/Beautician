bb5a780d13a510725366135eaf1486dd09e24602
Rahul Jain <talentediq@gmail.com>
2017-10-28 00:40:49 +0530

    First commit for Learning and making my website

---
+++ form.php
@@ -0,0 +1,15 @@
+<?php
+  // HTML -> PHP
+  // PHP -> Process Data -> Send To HTML
+  print_r($_POST);
+  echo "<br>";
+  print_r($_POST['anu']) ;
+  echo "<br>";
+  print_r($_POST['emailCrush']) ;
+  echo "<br>";
+  print_r($_POST['date']) ;
+  echo "<br>";
+  print_r($_POST['timing']) ;
+  echo "<br>";
+
+?>
